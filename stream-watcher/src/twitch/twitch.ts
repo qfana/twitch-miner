59445a2a930570dcda0a1903ed31f8792ca426a4
Nikita <qfana2123@gmail.com>
2025-07-13 01:39:52 +0300

    ADD LOGSы

--- stream-watcher/src/twitch/twitch.ts
+++ stream-watcher/src/twitch/twitch.ts
@@ -168,13 +168,18 @@ export class TwitchService implements ITwitchService {
 		const hasUnfinishedDrop = await page.$$eval('div[role="progressbar"]', (bars, targetSlug) => {
 			return bars.some(bar => {
 				const parentCard = bar.closest('div[data-a-target="drop-campaign-card"]');
+				console.log('parentCard', parentCard);
 				if (!parentCard) return false;
 				const gameName = parentCard.querySelector('p')?.textContent?.toLowerCase() || '';
+				console.log('gameName', gameName)
 				const transformed = gameName.replace(/[^a-z0-9]+/g, '-').replace(/(^-+|-+$)/g, '');
+				console.log('transformed', transformed)
 				const progress = parseInt(bar.getAttribute('aria-valuenow') || '0', 10);
+				console.log('progress', progress)
 				return transformed === targetSlug && progress < 100;
 			});
 		}, slug);
+
 
 		await page.close();
 		console.log(`[DEBUG] Для ${slug} незавершённый дроп найден:`, hasUnfinishedDrop);
