063c892c0a5aa24467c506757db55d4260a46fdf
Nikita <qfana2123@gmail.com>
2025-07-12 23:41:32 +0300

    ADD LOGSы

--- stream-watcher/src/twitch/twitch.ts
+++ stream-watcher/src/twitch/twitch.ts
@@ -82,7 +82,7 @@ export class TwitchService implements ITwitchService {
 			});
 
 			await page.evaluate(() => window.scrollBy(0, window.innerHeight));
-			await page.waitForTimeout(500);
+			await new Promise(resolve => setTimeout(resolve, 500));
 		}
 
 		await page.evaluate(() => window.scrollBy(0, window.innerHeight));
