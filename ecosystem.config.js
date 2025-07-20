94399499647f46f993652ef74458a21682b4f3eb
Nikita <qfana2123@gmail.com>
2025-07-21 02:35:20 +0300

    fix pm2 ecosystem cfg

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'tg-bot',
+      cwd: './',
+      script: 'pnpm',
+      args: 'exec ts-node -r tsconfig-paths/register bot/src/bot.ts',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'twitch-miner',
+      cwd: './',
+      script: 'pnpm',
+      args: 'exec ts-node -r tsconfig-paths/register stream-watcher/src/main.ts',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
