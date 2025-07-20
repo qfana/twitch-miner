fee0e710e6b650e201949bb316b1bba92d26eb18
Nikita <qfana2123@gmail.com>
2025-07-21 02:28:54 +0300

    fix pm2 ecosystem cfg

--- exosystem.config.js
+++ exosystem.config.js
@@ -1,21 +1,28 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
-      name: 'twitch-miner',
-      script: 'stream-watcher/src/main.ts',
-      interpreter: 'node',
-      interpreter_args: '-r ts-node/register',
+      name: 'tg-bot',
+      // Команда, которую запускает PM2
+      script: 'pnpm',
+      // Аргументы: "exec ts-node bot/src/bot.ts"
+      args: 'exec ts-node bot/bot.ts',
+      // Окружение
       env: {
         NODE_ENV: 'production',
+        TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
+        // ...прочие переменные
       },
     },
     {
-      name: 'tg-bot',
-      script: 'bot/bot.ts',
-      interpreter: 'node',
-      interpreter_args: '-r ts-node/register',
+      name: 'twitch-miner',
+      script: 'pnpm',
+      args: 'exec ts-node stream-watcher/src/main.ts',
       env: {
         NODE_ENV: 'production',
+        TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+        TWITCH_APP_TOKEN: process.env.TWITCH_APP_TOKEN,
+        // ...прочие
       },
     },
   ],
