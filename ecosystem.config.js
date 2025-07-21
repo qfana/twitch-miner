cea829dda3490699db3accc8f4007e96b7246103
Nikita <qfana2123@gmail.com>
2025-07-21 23:24:43 +0300

    del sreens

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,20 @@
-// ecosystem.config.js
 module.exports = {
   apps: [
-    {
-      name: 'twitch-miner',
-      cwd: './',
-      script: 'pnpm',
-      args: 'exec ts-node stream-watcher/src/main.ts',
-      env_file: '.env',
-    },
     {
       name: 'tg-bot',
       cwd: './',
       script: 'pnpm',
-      args: 'exec ts-node bot/bot.ts',
-      env_file: '.env',
+      args: 'exec ts-node -P tsconfig.json bot/bot.ts',
+      watch: false,
+      env_file: '.env'
     },
-  ],
+    {
+      name: 'twitch-miner',
+      cwd: './',
+      script: 'pnpm',
+      args: 'exec ts-node -P tsconfig.json stream-watcher/src/main.ts',
+      watch: false,
+      env_file: '.env'
+    }
+  ]
 };
