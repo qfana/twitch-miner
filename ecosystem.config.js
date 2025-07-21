177561a37d34672b3978afbe49e7dc06a887903c
Nikita <qfana2123@gmail.com>
2025-07-21 22:46:15 +0300

    try use ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,19 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
-    {
-      name: 'tg-bot',
-      cwd: './',
-      script: 'pnpm',
-      args: 'exec ts-node bot/bot.ts',
-      watch: false,
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
     {
       name: 'twitch-miner',
       cwd: './',
       script: 'pnpm',
       args: 'exec ts-node stream-watcher/src/main.ts',
-      watch: false,
-      env: {
-        NODE_ENV: 'production',
-      },
+      env_file: '.env',
+    },
+    {
+      name: 'tg-bot',
+      cwd: './',
+      script: 'pnpm',
+      args: 'exec ts-node bot/bot.ts',
+      env_file: '.env',
     },
   ],
 };
